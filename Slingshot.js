0fc804890f77879d7e4fce3e25c4a85b6b44fd14
Anu-2007 <reshrajeeve@gmail.com>
2021-02-08 15:49:29 +0530

    Anamika has made changes to this file

--- Slingshot.js
+++ Slingshot.js
@@ -1,4 +1,4 @@
-class SlingShot{
+ class SlingShot{
     constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
@@ -6,6 +6,11 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
+
+     this.sling1 = loadImage("sprites/sling1.png");
+     this.sling2 = loadImage("sprites/sling2.png");
+     this.sling3 = loadImage("sprites/sling3.png");
+
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -16,12 +21,42 @@ class SlingShot{
     }
 
     display(){
-        if(this.sling.bodyA){
+        
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+        
+        if(this.sling.bodyA)
+        {
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+           stroke((48,22,8));  // rgb is a color function(red,green,blue)
+           
+           
+           push();
+           if(pointA.x<200)
+           {
+            strokeWeight(7);
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+            image(this.sling3, pointA.x-30, pointA.y-15,50,10);
+          }
+           else
+           {
             strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+            image(this.sling3, pointA.x-30, pointA.y-15,50,10);
         }
+           pop();
+    }
+
+            
+            
+           
+          
+    
+        
+       
     }
     
-}
+};
